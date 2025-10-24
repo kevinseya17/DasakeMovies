e87541bee3f78ae23879635f62951dabf29f78f4
santiagobedon <santiago.bedon@correounivalle.edu.co>
2025-10-24 13:13:51 -0500

    autorizacion de un unico front

--- index.ts
+++ index.ts
@@ -11,7 +11,6 @@ const app = express();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://dasake-front-yc4x.vercel.app",
     "https://dasake-front.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
